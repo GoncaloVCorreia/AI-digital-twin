cc9735b8dcced2bc13e5f0a2a185124a87cab9f4
Goncalo Correia <95139684+GoncaloVCorreia@users.noreply.github.com>
2025-10-13 17:12:51 +0100

    Update chat_graph.py

--- src/chatbot/chat_graph.py
+++ src/chatbot/chat_graph.py
@@ -2,7 +2,6 @@
 from langgraph.graph import StateGraph, START, END
 from langgraph.graph.message import add_messages
 from langgraph.checkpoint.postgres import PostgresSaver
-from langgraph.checkpoint.memory import MemorySaver
 from langchain_core.messages import AIMessage
 from typing import Annotated, TypedDict, List, Dict
 from typing_extensions import NotRequired
@@ -14,15 +13,9 @@ class ChatState(TypedDict):
     messages: Annotated[List, add_messages]
     last_ai_message: NotRequired[str]
     
-def make_checkpointer():
-    if os.getenv("USE_MEMORY_CHECKPOINTER") == "1":
-        return MemorySaver()
-    url = DATABASE_URL
-    return PostgresSaver.from_conn_string(url)
-    
 class ChatGraphRunner:
     def __init__(self, llm):
-        self._memory_cm = make_checkpointer()
+        self._memory_cm = PostgresSaver.from_conn_string(DATABASE_URL)
         self.memory = self._memory_cm.__enter__()
         self.memory.setup()
 
