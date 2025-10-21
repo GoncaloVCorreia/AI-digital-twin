5599a69f256c736746504aa124b8470eb6fa0096
GoncaloVCorreia <goncalovasconceloscorreia@gmail.com>
2025-10-21 09:24:43 +0100

    Secrete Key

--- app/config.py
+++ app/config.py
@@ -20,6 +20,7 @@ class Settings(BaseSettings):
     ACCESS_TOKEN_EXPIRE_MINUTES: int = 60 * 24 * 8  # 8 days
     ALGORITHM: str = "HS256"
     GROQ_API_KEY: str
+    SECRETE_API_KEY: str
     
     # Database
     DATABASE_URL: Optional[str] = "sqlite:///./task_management.db"
