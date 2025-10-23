90a51841212feec139519bc56db61b33ec256b70
Goncalo Correia <95139684+GoncaloVCorreia@users.noreply.github.com>
2025-10-23 19:49:38 +0100

    Update groq_config.py

--- src/configs/groq_config.py
+++ src/configs/groq_config.py
@@ -3,7 +3,7 @@ from pydantic import BaseModel
 
 
 class ConfigGroq(BaseModel):
-    model_name: str = "openai/gpt-oss-20b"
+    model_name: str = "openai/gpt-oss-120b"
     temperature: float = 0.0
     max_tokens: int = 512
     
